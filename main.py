ef1b6edb68c50262b064bebb50628e6587b007c8
Rezki Abderrahman <rezkiabderrahman@gmail.com>
2022-08-23 21:07:54 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import cv2
+import pytesseract
+# cam = cv2.VideoCapture(0)   # 0 -> index of camera
+# s, img = cam.read()
+# if s:    # frame captured without any errors
+#     cv2.namedWindow("cam-test")
+#     cv2.imshow("cam-test",img)
+#     cv2.waitKey(0)
+#     cv2.destroyWindow("cam-test")
+#     cv2.imwrite("images/filename.jpg",img) #save image
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow('cam-test')
+while True:
+    s, img = cam.read()
+    if not s:
+        print('fail')
+        break
+    cv2.imshow('cam-test', img)
+    k  = cv2.waitKey(1)
+    if k%256 == 27:
+        print('Closing')
+        break
+    elif k%256  == 32:
+        cv2.imwrite("images/filename.jpg",img)
+        print('Saved')
+
+cam.release()
+
+cv2.destroyAllWindows()
+img= cv2.imread("images/filename.jpg")
+
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+text = pytesseract.image_to_string(img)
+print(text)
